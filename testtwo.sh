64cb55394ce4d03ea4e9b2aaddcf94669712faaf
Sem Schreuder <semschreuder@hotmail.com>
2014-10-02 10:43:20 +0200

    hoi

--- testtwo.sh
+++ testtwo.sh
@@ -6,14 +6,14 @@
 #
 
 
-if [ $# -lt 1 ]; then
-	echo "geef een bronfile"
+if [ $# -lt 2 ]; then
+	echo "geef een bronfile en aantal slices"
 	exit
 fi
 
 SAMPLE=$(soxi -s $1)
-FILENAME=$1
-LENGTE=$(echo "scale=0; $SAMPLE/8" | bc)
+FILENAME="$1"
+LENGTE=$(echo "scale=0; $SAMPLE/$2" | bc)
 
 for c in `seq 1 8`;
 do
@@ -21,7 +21,7 @@ do
 	END=$(echo "scale=0; $LENGTE*$c" | bc)
 	WOW=`basename $c`
 
-	sox $FILENAME ~/Desktop/output/${WOW}$1 trim =${START}s =${END}s
+	sox "$FILENAME" ${WOW}$1 trim =${START}s =${END}s
 
 	echo "start is $START"
 	echo "end is $END"
@@ -30,4 +30,5 @@ do
 done
 
 echo "KLAAR"
+
 
